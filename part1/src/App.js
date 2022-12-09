0afefc28f8a446f3c199230ef4c36854cea750cb
Kairoz <ivykairos@gmail.com>
2022-12-08 19:40:16 -0600

    Exercise 1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,3 +1,25 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  );
+}
+
+const Content = (props) => {
+  return (
+    props.parts.map((val) => {
+      return (
+        <p>{val['part_name'] + " " + val['exercises']}</p>
+      );
+    })
+  );
+}
+
+const Total = (props) => {
+  return (
+    <p>{"Number of exercises " + props.exercises.reduce((a, b) => a + b, 0)}</p>
+  );
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +31,15 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={
+        [
+          {part_name: part1, exercises: exercises1},
+          {part_name: part2, exercises: exercises2},
+          {part_name: part3, exercises: exercises3}
+        ]}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
 }
