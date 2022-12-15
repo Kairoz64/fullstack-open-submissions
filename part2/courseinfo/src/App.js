364f5eac7f984d61884a595e3e068ffafe881ec0
Kairoz <ivykairos@gmail.com>
2022-12-14 20:59:25 -0600

    Exercise 2.5 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,41 +1,4 @@
-const Header = ({course}) => {
-  return (
-    <h1>{course}</h1>
-  );
-}
-
-const Part = ({part}) => {
-  return (
-    <p>{part.name + " " + part.exercises}</p>
-  );
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map((part) => {
-        return (
-          <Part part={part} key={part.id}/>
-        )
-      })}
-    </div>
-  );
-}
-
-const Total = ({parts}) => {
-  return (
-    <p><b>{"Number of exercises " + parts.reduce((a, b) => a + b.exercises, 0)}</b></p>
-  );
-}
-const Course = ({course}) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-}
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
