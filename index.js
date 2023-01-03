10e50f57ee6dba18f7075d00d430ce904be430cc
Kairoz <ivykairos@gmail.com>
2023-01-02 21:24:12 -0600

    Exercise 3.2 done

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (req, res) => {
 	res.json(persons);
 });
 
+app.get('/info', (req, res) => {
+	let numberOfPeople = persons.length
+	let date = new Date();
+	res.send(`<p>Phonebook has info for ${numberOfPeople} people</p><p>${date}</p>`);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
