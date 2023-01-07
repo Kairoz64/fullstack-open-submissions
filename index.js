99dc3a5855c20ac9d732c7d7c9684f1e4ae057f4
Kairoz <ivykairos@gmail.com>
2023-01-06 22:30:25 -0600

    Exercise 3.7 done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = express()
+const morgan = require('morgan');
 
+const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max) + 1;
