bb7a0050e7971bf6963798335a2698d362b82318
Kairoz <ivykairos@gmail.com>
2023-01-08 12:38:13 -0600

    Exercise 3.11 done

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ morgan.token('body', (req, res) => {
   }
 })
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
