8a41af711c599d02f53e1511a045cc3986f1480c
Kairoz <ivykairos@gmail.com>
2023-01-15 11:43:31 -0600

    Exercise 3.22 done, enforcing eslint rules

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const Person = require('./models/person')
+const Person = require('./models/person');
 
-const app = express()
+const app = express();
 
-morgan.token('body', (req, res) => {
+morgan.token('body', (req) => {
   if (req.method === 'POST') {
     return JSON.stringify(req.body);
   }
-})
+});
 
-app.use(express.static('build'))
-app.use(cors())
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'));
+app.use(cors());
+app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  { 'id': 1,
+    'name': 'Arto Hellas',
+    'number': '040-123456'
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  { 'id': 2,
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523'
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    'id': 3,
+    'name': 'Dan Abramov',
+    'number': '12-43-234345'
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+  {
+    'id': 4,
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122'
   }
-]
+];
 
 app.get('/info', (req, res) => {
-  let numberOfPeople = persons.length
+  let numberOfPeople = persons.length;
   let date = new Date();
   res.send(`<p>Phonebook has info for ${numberOfPeople} people</p><p>${date}</p>`);
 });
@@ -49,29 +47,29 @@ app.get('/info', (req, res) => {
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
     res.json(persons);
-  })
+  });
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
-	const id = req.params.id;
+  const id = req.params.id;
 
   Person.findById(id).then((p) => {
     if (p) {
       res.json(p);
     } else {
-      res.status(404).end()
+      res.status(404).end();
     }
   })
-  .catch(err => next(err))
+    .catch(err => next(err));
 });
 
 app.delete('/api/persons/:id', (req, res, next) => {
-	const id = req.params.id;
+  const id = req.params.id;
 
-  Person.findByIdAndRemove(id).then(p => {
-    res.status(204).end()
+  Person.findByIdAndRemove(id).then(() => {
+    res.status(204).end();
   })
-  .catch(err => next(err))
+    .catch(err => next(err));
 });
 
 app.put('/api/persons/:id', (req, res, next) => {
@@ -87,23 +85,23 @@ app.put('/api/persons/:id', (req, res, next) => {
   const person = {
     name: body.name,
     number: body.number
-  }
+  };
 
-  Person.findByIdAndUpdate(id, person, {new: true, runValidators: true, context: 'query'}).then((p) => {
+  Person.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' }).then((p) => {
     if (p === null) {
       return res.status(400).json({
         error: 'resource doesn\'t exist',
         code: 2
-      })
+      });
     }
 
     res.json(p);
   })
-  .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
 app.post('/api/persons', (req, res, next) => {
-  const body = req.body
+  const body = req.body;
 
   if (!body.name || !body.number) {
     return res.status(400).json({
@@ -114,9 +112,9 @@ app.post('/api/persons', (req, res, next) => {
   const person = new Person({
     name: body.name,
     number: body.number
-  })
+  });
 
-  Person.findOne({name: person.name}).then((duplicate) => {
+  Person.findOne({ name: person.name }).then((duplicate) => {
     if (duplicate) {
       return res.status(400).json({
         error: 'duplicate name'
@@ -126,35 +124,35 @@ app.post('/api/persons', (req, res, next) => {
     person.save().then(p => {
       res.json(p);
     })
-    .catch(error => next(error))
-  })
+      .catch(error => next(error));
+  });
 });
 
 const errorHandler = (err, req, res, next) => {
-  console.error(err.message)
+  console.error(err.message);
 
   if (err.name === 'CastError') {
-    return res.status(400).json({error: 'malformated id'})
-  } 
+    return res.status(400).json({ error: 'malformated id' });
+  }
 
-  else if (err.name == 'ValidationError') {
+  else if (err.name === 'ValidationError') {
     let messages = [];
-    const keys = Object.keys(err.errors) 
+    const keys = Object.keys(err.errors);
     if (keys.includes('name')) {
       messages.push('Length of name must be 3 characters minimum');
     }
     if (keys.includes('number')) {
       messages.push('Invalid phone');
     }
-    return res.status(400).json({ error: 'Validation Error: ' + messages.join(', ')})
+    return res.status(400).json({ error: 'Validation Error: ' + messages.join(', ') });
   }
 
   next(err);
-}
+};
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
--- mongo.js
+++ mongo.js
@@ -2,46 +2,46 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 if (process.argv.length < 3) {
-	console.log('Please provide the password as an argument: node mongo.js <password>');
-	process.exit(1);
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1);
 }
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://kairoz:${password}@phonebook-db.fy5dxly.mongodb.net/phonenookApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://kairoz:${password}@phonebook-db.fy5dxly.mongodb.net/phonenookApp?retryWrites=true&w=majority`;
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String
-})
+  name: String,
+  number: String
+});
 
 const Person = mongoose.model('Person', personSchema);
 
 mongoose
-	.connect(url)
-	.then((res) => {
-		console.log('connected');
-		if (process.argv.length < 4) {
-			return Person.find({}).then(res => {
-				console.log('phonebook:');
-				res.forEach(p => {
-					console.log(`${p.name} ${p.number}`);
-				})
-			});
-		}
+  .connect(url)
+  .then(() => {
+    console.log('connected');
+    if (process.argv.length < 4) {
+      return Person.find({}).then(res => {
+        console.log('phonebook:');
+        res.forEach(p => {
+          console.log(`${p.name} ${p.number}`);
+        });
+      });
+    }
 
-		else {
-			const person = new Person({
-				name: process.argv[3],
-				number: process.argv[4]
-			})
-			return person.save()
-		}
-	})
-	.then((p) => {
-		if (process.argv.length > 3) {
-			console.log(`added ${p.name} number ${p.number} to phonebook`);
-		}
-		return mongoose.connection.close()
-	})
-	.catch((err) => console.log(err));
+    else {
+      const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+      });
+      return person.save();
+    }
+  })
+  .then((p) => {
+    if (process.argv.length > 3) {
+      console.log(`added ${p.name} number ${p.number} to phonebook`);
+    }
+    return mongoose.connection.close();
+  })
+  .catch((err) => console.log(err));
