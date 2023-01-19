da76e009208a0a1334554eacca32eb2c53734990
Kairoz <ivykairos@gmail.com>
2023-01-19 17:09:55 -0600

    Exercise 4.5 done, implemented favorite blog function and tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,27 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
+const favoriteBlog = (blogs) => {
+
+  if (blogs.length === 0) {
+    return undefined;
+  }
+
+  let largestNumberIndex = 0;
+  let largestNumber = 0;
+
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > largestNumber) {
+      largestNumber = blogs[i].likes;
+      largestNumberIndex = i;
+    }
+  }
+
+  return blogs[largestNumberIndex];
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
