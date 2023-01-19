dd4ebd86c1b1a8574dcbbd57045c30253e8b7171
Kairoz <ivykairos@gmail.com>
2023-01-19 16:25:02 -0600

    Exercise 4.4 done, totalLikes function and tests implemented

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((a, v) => {
+    return a + v.likes;
+  }, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
