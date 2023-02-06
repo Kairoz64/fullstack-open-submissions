ed6ab437cabeab92729f954d34f0d0fc7b0f9c94
Kairoz <ivykairos@gmail.com>
2023-02-05 19:51:53 -0600

    Exercise 5.1, implemented a barebones login functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
+const Login = ({
+    username,
+    password,
+    handleUsernameChange,
+    handlePasswordChange,
+    handleSubmit
+  }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+}
+
+const Blogs = ({blogs}) => {
+  return(
+    <>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </>
+  );
+}
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +59,32 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username, password
+      });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (e) {
+      console.log('Wrong credentials')
+    }
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null && 
+        <Login 
+          username={username}
+          password={password}
+          handleUsernameChange={({target}) => {setUsername(target.value)}}
+          handlePasswordChange={({target}) => {setPassword(target.value)}}
+          handleSubmit={handleLogin}
+      />}
+      {user !== null && <Blogs blogs={blogs}/>}
     </div>
   )
 }
