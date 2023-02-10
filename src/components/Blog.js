3496b4b7b41a06fbb959fd5cdeb4ae0c44bf33d6
Kairoz <ivykairos@gmail.com>
2023-02-09 20:14:23 -0600

    Exercise 5.7 done, you can toggle blog details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+	const [visible, setVisible] = useState(false);
+	const shownWhenVisible = { display: visible ? '' : 'none' };
+
+	const toggleVisibility = () => {
+		setVisible(!visible);
+	};
+
+	return (
+		<div className='blog-container'>
+			<div>
+				{blog.title} {blog.author}
+				<button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+			</div>
+			<div style={shownWhenVisible}>
+				<div>{blog.url}</div>
+				<div>
+					likes {blog.likes}
+					<button>like</button>
+				</div>
+				<div>{blog.user.name}</div>
+			</div>
+		</div>
+	);
+};
 
 export default Blog;
