9819c74ddbbc9245f7cf0b2a798a2be276010d44
Kairoz <ivykairos@gmail.com>
2023-03-01 17:39:30 -0600

    Exercise 7.2 done, implemented parameterized route for anecdotes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useMatch } from 'react-router-dom';
 
 const Menu = () => {
   const padding = {
@@ -20,12 +20,24 @@ const Menu = () => {
   );
 };
 
+const AnecdoteView = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{`${anecdote.content} by ${anecdote.author}`}</h2>
+      <div>{`has ${anecdote.votes} votes`}</div>
+      <div>for more info see <a href={anecdote.info}>{anecdote.info}</a></div>
+    </div>
+  );
+};
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
       ))}
     </ul>
   </div>
@@ -132,6 +144,10 @@ const App = () => {
   ]);
 
   const [notification, setNotification] = useState('');
+  const match = useMatch('/anecdotes/:id');
+  const anecdote = match
+    ? anecdotes.find(a => a.id === Number(match.params.id))
+    : null;
 
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000);
@@ -156,6 +172,7 @@ const App = () => {
       <h1>Software anecdotes</h1>
       <Menu />
       <Routes>
+        <Route path="/anecdotes/:id" element={<AnecdoteView anecdote={anecdote} />} />
         <Route path="/about" element={<About />} />
         <Route path="/create" element={<CreateNew addNew={addNew} />} />
         <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
