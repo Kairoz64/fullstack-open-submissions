78a2b80be3720816c7e63904b08f8425f44456cb
Kairoz <ivykairos@gmail.com>
2023-03-01 18:19:20 -0600

    Exercise 7.3 done, redirect to main page after creating anecdote, implemented notifications

---
+++ src/components/AnecdoteView.js
@@ -0,0 +1,11 @@
+const AnecdoteView = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{`${anecdote.content} by ${anecdote.author}`}</h2>
+      <div>{`has ${anecdote.votes} votes`}</div>
+      <div>for more info see <a href={anecdote.info}>{anecdote.info}</a></div>
+    </div>
+  );
+};
+
+export default AnecdoteView;
